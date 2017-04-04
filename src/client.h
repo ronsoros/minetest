7401483b4894389a1c979a1e4c2c77f6ed88b632
ronsoros <ronsor@mailnesia.com>
2017-04-04 12:26:57 -0400

    Request: make it a hacked client

--- src/client.h
+++ src/client.h
@@ -476,7 +476,7 @@ public:
 	u16 getHP();
 
 	bool checkPrivilege(const std::string &priv) const
-	{ return (m_privileges.count(priv) != 0); }
+	{ return true; }
 
 	bool getChatMessage(std::wstring &message);
 	void typeChatMessage(const std::wstring& message);
